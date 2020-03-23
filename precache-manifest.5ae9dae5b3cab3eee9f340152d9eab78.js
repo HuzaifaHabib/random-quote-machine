9a1e3a96c8e10e5998c2e2e82e82f6539932dbe0
Huzaifa Habib <huzaifa.dj@gmail.com>
2020-03-23 12:09:49 +0500

    Updates

---
+++ precache-manifest.5ae9dae5b3cab3eee9f340152d9eab78.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf21c56f6af2c2e80be5834e6ee9876",
+    "url": "/random-quote-machine/./index.html"
+  },
+  {
+    "revision": "26e0a4e027ef433d1d7a",
+    "url": "/random-quote-machine/./static/css/2.4d595903.chunk.css"
+  },
+  {
+    "revision": "255142f56fad30796b26",
+    "url": "/random-quote-machine/./static/css/main.8477c57e.chunk.css"
+  },
+  {
+    "revision": "26e0a4e027ef433d1d7a",
+    "url": "/random-quote-machine/./static/js/2.36bfeb9f.chunk.js"
+  },
+  {
+    "revision": "255142f56fad30796b26",
+    "url": "/random-quote-machine/./static/js/main.a95a6127.chunk.js"
+  },
+  {
+    "revision": "7c7c0ab89e0133ce8bff",
+    "url": "/random-quote-machine/./static/js/runtime~main.6df6bf65.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/random-quote-machine/./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/random-quote-machine/./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/random-quote-machine/./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/random-quote-machine/./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/random-quote-machine/./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
